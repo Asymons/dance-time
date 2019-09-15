0b9d64d0a705311299a829072b4d1a93683d3f13
Asymons <alessiosymons@gmail.com>
2019-09-15 01:45:44 -0400

    new-result-page

--- client/src/screens/ResultPage.js
+++ client/src/screens/ResultPage.js
@@ -3,6 +3,7 @@ import { ContentWrapper, Subtitle, Title, VideoRow, VideoWrapper, Wrapper } from
 import ReactPlayer from 'react-player';
 import { idToName } from '../helpers/nameHelper';
 import { leaderboards } from '../helpers/firebase';
+import { Cell, Label, Pie, PieChart } from 'recharts';
 
 const ResultPage = ({ match }) => {
     const [userData, setUserData] = useState({});
@@ -16,6 +17,18 @@ const ResultPage = ({ match }) => {
             setUserData(user);
         })
     }, []);
+    const similarityScore = [
+        {
+            name: 'Similarity',
+            value: userData.score ? userData.score : 0,
+            color: '#FFD300'
+        },
+        {
+            name: 'Improvement',
+            value: userData.score ? 100 - userData.score : 100,
+            color: '#D1D1D1'
+        }
+    ];
     return (
       <Wrapper>
           <ContentWrapper>
@@ -23,20 +36,41 @@ const ResultPage = ({ match }) => {
               <VideoRow>
                   <VideoWrapper>
                       <Subtitle>You</Subtitle>
-                      <video id="videoPlayer" controls>
+                      <video style={{width: 480}} id="videoPlayer" controls>
                           <source src={`http://localhost:3000/video/${id}-before.mp4`} type="video/mp4" />
                       </video>
                   </VideoWrapper>
                   <VideoWrapper>
                       <Subtitle>Soon to be you</Subtitle>
-                      <video id="videoPlayer" controls>
+                      <video style={{width: 480}} id="videoPlayer" controls>
                           <source src={`http://localhost:3000/video/${id}-after.mp4`} type="video/mp4" />
                       </video>
                   </VideoWrapper>
               </VideoRow>
               {
                   userData.score && (
-                      <Subtitle>Similarity Score: {userData.score}%</Subtitle>
+                      <PieChart width={300} height={300}>
+                          <Pie
+                              data={similarityScore}
+                              dataKey="value"
+                              nameKey="name"
+                              cx="50%"
+                              cy="50%"
+                              innerRadius="60%"
+                              outerRadius="80%"
+                              startAngle={180}
+                              endAngle={-180}
+                          >
+                              {
+                                  similarityScore.map((entry, index) => (
+                                      <Cell key={`cell-${index}`} fill={entry.color}/>
+                                  ))
+                              }
+                              <Label width={30} position="center">
+                                  { `Similarity Score: ${userData.score}%` }
+                              </Label>
+                          </Pie>
+                      </PieChart>
                   )
               }
           </ContentWrapper>
