48a0a080896afcae68d00b269474d13767930939
Asymons <alessiosymons@gmail.com>
2019-09-15 01:38:30 -0400

    added similarity

--- client/src/screens/ResultPage.js
+++ client/src/screens/ResultPage.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ContentWrapper, Subtitle, Title, VideoRow, VideoWrapper, Wrapper } from '../styles/ResultPage';
 import ReactPlayer from 'react-player';
 import { idToName } from '../helpers/nameHelper';
+import { leaderboards } from '../helpers/firebase';
 
 const ResultPage = ({ match }) => {
+    const [userData, setUserData] = useState({});
     const { id } = match.params;
     const name = idToName(id);
+    useEffect(() => {
+        leaderboards.once('value').then(snapshot => {
+            const data = snapshot.val();
+            const user = data[Object.keys(data).filter((key) => data[key].name === name)[0]];
+            console.log(user);
+            setUserData(user);
+        })
+    }, []);
     return (
       <Wrapper>
           <ContentWrapper>
@@ -13,14 +23,22 @@ const ResultPage = ({ match }) => {
               <VideoRow>
                   <VideoWrapper>
                       <Subtitle>You</Subtitle>
-                      <ReactPlayer width={480} url={'https://www.youtube.com/watch?v=0Kj3wWKjMSQ'} controls/>
+                      <video id="videoPlayer" controls>
+                          <source src={`http://localhost:3000/video/${id}-before.mp4`} type="video/mp4" />
+                      </video>
                   </VideoWrapper>
                   <VideoWrapper>
                       <Subtitle>Soon to be you</Subtitle>
-                      <ReactPlayer width={480} url={'https://www.youtube.com/watch?v=0Kj3wWKjMSQ'} controls/>
+                      <video id="videoPlayer" controls>
+                          <source src={`http://localhost:3000/video/${id}-after.mp4`} type="video/mp4" />
+                      </video>
                   </VideoWrapper>
               </VideoRow>
-              <Subtitle>Similarity Score: 80%</Subtitle>
+              {
+                  userData.score && (
+                      <Subtitle>Similarity Score: {userData.score}%</Subtitle>
+                  )
+              }
           </ContentWrapper>
       </Wrapper>
     );
